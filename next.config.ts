3b3f1b85496321a5d9f684cc6d668c038c8ad69d
Muhammad Ayan Aziz <ayaan.aziz2007@gmail.com>
2026-09-01 11:44:57 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   cacheComponents: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/chatbot",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
